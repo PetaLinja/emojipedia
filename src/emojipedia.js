9ca9b088505926256faa9d45f32916579e2b8e0f
Peter <pjlin13@gmail.com>
2021-08-31 10:27:00 -0400

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,32 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "“Commonly used to convey irony, sarcasm, joking, or a sense of goofiness or silliness. Its intent can be similar to the bemused Oh well! of 🤷 Person Shrugging or the shruggie emoticon, ¯\_(ツ)_/¯."
+  },
+  {
+    id: 2,
+    emoji: "🤡",
+    name: "Clown Face",
+    meaning:
+      "A circus or birthday clown displayed with white face makeup, a red nose, exaggerated eyes and smile, and two tufts of hair (red or blue). Commonly used as comment on TikTok or other social media platforms to indicate that a person is foolish, idiotic and/or selfish."
+  },
+  {
+    id: 3,
+    emoji: "🙂",
+    name: "Slightly Smiling Face",
+    meaning:
+      "A yellow face with simple, open eyes and a thin, closed smile. Conveys a wide range of positive, happy, and friendly sentiments. Its tone can also be patronizing, passive-aggressive, or ironic, as if saying This is fine when it’s really not."
+  },
+  {
+    id: 4,
+    emoji: "🤤",
+    name: "Drooling Face",
+    meaning:
+      "A yellow face, usually shown with closed eyes and raised eyebrows, with saliva drooling from one corner of its mouth. Often used to show desire for a person (sexy, attractive) or object (e.g., delicious food). May also represent someone is tired or sleeping."
+  }
+];
+
+export default emojipedia;
